e53bc83467a2d2b59ce5b5f0a4f288694eb4559c
FurbaSherpa <37233795+FurbaSherpa@users.noreply.github.com>
2018-04-06 11:48:33 -0400

    Add files via upload

--- Quartet/Login.aspx.cs
+++ Quartet/Login.aspx.cs
@@ -4,11 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+    protected void Btn_login(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyOwnConnectionString"].ConnectionString);
+        conn.Open();
+        string checkEmail = "SELECT COUNT(*) FROM Users WHERE UserEmail='" + loginEmail_txt.Text + "'";
+        SqlCommand com = new SqlCommand(checkEmail, conn);
+        int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+        conn.Close();
+        if (temp == 1 )
+        {
+            conn.Open();
+            string checkPasswordQuery = "SELECT UserPassword FROM Users where userEmail='" + loginEmail_txt.Text + "'";
+            SqlCommand Passcom = new SqlCommand(checkPasswordQuery, conn);
+            string password = Passcom.ExecuteScalar().ToString().Replace(" ", "");
+            if (password == passwordLogin_txt.Text)
+            {
+                
+                Response.Redirect("LoginConfirmation.aspx");
+            }
+            else
+            {
+                Response.Write("Incorrect Login Information");
+            }
+        }
+        else
+        {
+            Response.Write("Incorrect Login Information");
+        }
 
     }
 }
